ce3bb133946f3755d09bce5a8d89434b3b5f6c80
Bastien <bastien.guerry@data.gouv.fr>
2019-04-17 15:56:13 +0200

    Bump dependency version

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: LICENSES/EPL-2.0.txt
 
-(defproject retweet "0.3"
+(defproject retweet "0.3.1"
   :url "https://github.com/bzg/retweet"
   :license {:name "Eclipse Public License v2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [twttr "3.2.1"]]
+                 [twttr "3.2.2"]]
   :description "Bot to retweet tweets from users with hashtags."
   :main retweet.core
   :profiles {:uberjar {:aot :all}})
